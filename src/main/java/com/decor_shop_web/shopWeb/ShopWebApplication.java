f9c08753297738f97ee34f8a6863c32481839973
sofiasales02 <sofiasaleswk@gmail.com>
2024-08-15 20:34:25 -0300

    initial commit

---
+++ src/main/java/com/decor_shop_web/shopWeb/ShopWebApplication.java
@@ -0,0 +1,13 @@
+package com.decor_shop_web.shopWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decor_shop_web/shopWeb/ShopWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decor_shop_web.shopWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
